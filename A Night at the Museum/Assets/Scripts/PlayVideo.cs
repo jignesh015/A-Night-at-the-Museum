5eb68b005ff628decdc245f7f3ec2170d72a138b
Jignesh Patel <patel.jignesh705@gmail.com>
2018-10-21 00:40:52 +0530

    Added progress slider to play buttons

--- A Night at the Museum/Assets/Scripts/PlayVideo.cs
+++ A Night at the Museum/Assets/Scripts/PlayVideo.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.UI;
 
 public class PlayVideo : MonoBehaviour {
 
@@ -11,8 +12,25 @@ public class PlayVideo : MonoBehaviour {
 
 	public GameObject[] playButtons;
 
+	public Material playMaterial;
+
+	public Material pauseMaterial;
+
+	public Canvas sliderCanvas;
+
+	public Slider slider;
+
+	private float enterTime;
+	private bool pointerFlag = false;
+	private int buttonIndex;
+
+	private Vector3 pausePos = new Vector3 (0f, -0.5f, 0f);
+	private Vector3 playPos = new Vector3 (0f, 0.5f, 0f);
+
 	// Use this for initialization
 	void Start () {
+		slider.maxValue = 1.5f;
+
 		for (int i = 0; i < noOfDisplay; i++) {
 			GameObject display = videoplayers [i].transform.parent.GetChild(1).gameObject;
 			display.SetActive (false);
@@ -21,17 +39,36 @@ public class PlayVideo : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (pointerFlag) {
+			//Sets the progress slider value
+			float sliderValue = Time.time - enterTime;
+			slider.value = sliderValue;
+
+			if ((Time.time - enterTime) > slider.maxValue) {
+				PlayButtonClicked (buttonIndex);
+				pointerFlag = false;
+			}
+		}
+	}
+
+	public void pointerEnter(int index) {
+		pointerFlag = true;
+		enterTime = Time.time;
+		sliderCanvas.gameObject.SetActive (true);
+		buttonIndex = index;
+	}
+
+	public void pointerExit() {
+		pointerFlag = false;
+		sliderCanvas.gameObject.SetActive (false);
 	}
 
 	//Play video on click of Play button
 	public void PlayButtonClicked( int index) {
-		Debug.Log ("Clicked " + index);	
-		playButtons [index].SetActive (false);
-
 		GameObject display = videoplayers [index].transform.parent.GetChild(1).gameObject;
 		display.SetActive (true);
-		videoplayers [index].Play ();
+
+		TogglePause (index);
 	}
 
 	//Reset each video player when user moves
@@ -39,7 +76,8 @@ public class PlayVideo : MonoBehaviour {
 		for (int i = 0; i < noOfDisplay; i++) {
 			GameObject display = videoplayers [i].transform.parent.GetChild(1).gameObject;
 			display.SetActive (false);
-			playButtons [i].SetActive (true);
+			Renderer render =  playButtons[i].GetComponentsInChildren<Renderer>()[0];
+			render.material = playMaterial;
 
 			videoplayers [i].Stop ();
 		}
@@ -48,10 +86,17 @@ public class PlayVideo : MonoBehaviour {
 	public void TogglePause(int index) {
 		if (videoplayers [index].isPlaying) {
 			videoplayers [index].Pause ();
-			playButtons [index].SetActive (true);
+			Renderer render =  playButtons[index].GetComponentsInChildren<Renderer>()[0];
+			render.material = playMaterial;
+
+			playButtons [index].transform.position += playPos;
 		} else {
 			videoplayers [index].Play ();
-			playButtons [index].SetActive (false);
+			Renderer render =  playButtons[index].GetComponentsInChildren<Renderer>()[0];
+			render.material = pauseMaterial;
+		
+			playButtons [index].transform.position += pausePos;
 		}
 	}
+
 }
